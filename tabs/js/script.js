9ac779f591573ad797f24d0d3fcbae4cfcc3e502
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-22 23:13:04 +0200

    feat:tabs

---
+++ tabs/js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    //==================== TABS ==========================
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {//скрытие всех табов (контента)
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {//убираем класс активности у кнопок табов
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {//i = 0 === параметр по умолчанию => передаем первый слайд (функция показа таба)
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (e.target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+    //==================== //TABS ==========================
+});
+
+/* 
+Add in HTML
+<div class="tabcontainer">
+    <div class="tabheader">
+        <ul class="tabheader__items">
+            <li class="tabheader__item tabheader__item_active"></li>
+            <li class="tabheader__item"></li>
+        </ul>
+    </div>
+    <div class="tabcontent"></div>
+    <div class="tabcontent"></div>
+</div>
+*/
